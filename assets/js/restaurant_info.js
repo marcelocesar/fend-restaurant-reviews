089b6e735a0fb5b141b3c6644fca373138e76542
Marcelo Cesar <marcelop3@gmail.com>
2019-04-19 23:28:06 -0300

    responsive design

--- assets/js/restaurant_info.js
+++ assets/js/restaurant_info.js
@@ -133,12 +133,12 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
   const li = document.createElement('li');
-  const name = document.createElement('p');
+  const name = document.createElement('h3');
   name.innerHTML = review.name;
   li.appendChild(name);
 
   const date = document.createElement('p');
-  date.innerHTML = review.date;
+  date.innerHTML = `<time pubdate>${review.date}</time>`;
   li.appendChild(date);
 
   const rating = document.createElement('p');
